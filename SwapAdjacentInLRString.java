e4608feddac3e2296be5b9879c2ba474e76f1a4f
ervishnuj <110319808+ervishnuj@users.noreply.github.com>
2026-06-16 15:52:21 +0530

    Swap Adjacent in LR String
    In a string composed of 'L', 'R', and 'X' characters, like "RXXLRXRXL", a move consists of either replacing one occurrence of "XL" with "LX", or replacing one occurrence of "RX" with "XR". Given the starting string start and the ending string result, return True if and only if there exists a sequence of moves to transform start to result.
    
     
    
    Example 1:
    
    Input: start = "RXXLRXRXL", result = "XRLXXRRLX"
    Output: true
    Explanation: We can transform start to result following these steps:
    RXXLRXRXL ->
    XRXLRXRXL ->
    XRLXRXRXL ->
    XRLXXRRXL ->
    XRLXXRRLX
    Example 2:
    
    Input: start = "X", result = "L"
    Output: false
     
    
    Constraints:
    
    1 <= start.length <= 104
    start.length == result.length
    Both start and result will only consist of characters in 'L', 'R', and 'X'.

---
+++ SwapAdjacentInLRString.java
@@ -0,0 +1,22 @@
+
+class SwapAdjacentInLRString {
+    public boolean canTransform(String start, String result) {
+        int i=0;
+        int j=0;
+        int n=start.length();
+
+        while(i<n||j<n){
+            while(i<n&&start.charAt(i)=='X'){i++;}
+            while(j<n&&result.charAt(j)=='X'){j++;}
+
+            if((i==n)!=(j==n))return false;
+            if((i==n)&&(j==n))return true;
+            if(start.charAt(i)!=result.charAt(j))return false;
+            if(start.charAt(i)=='L'&&i<j)return false;
+            if(start.charAt(i)=='R'&&i>j)return false;
+            i++;
+            j++;
+        }
+        return true;
+    }
+}
