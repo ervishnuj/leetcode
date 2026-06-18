cb17ceec1d03cad275e71e23afe890b2e1c6ff7d
ervishnuj <ervishnuj@gmail.com>
2026-06-19 00:06:54 +0530

    Number of Students Doing Homework at a Given Time

---
+++ NumberOfStudentsDoingHomeworkAtAGivenTime.java
@@ -0,0 +1,12 @@
+public class NumberOfStudentsDoingHomeworkAtAGivenTime {
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int n=startTime.length;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if((endTime[i])>=queryTime&&startTime[i]<=queryTime){
+                count++;
+            }
+        }
+        return count;
+    }
+}
