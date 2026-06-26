02f34b399966800ac061d4d6f0b873bab61e2789
ervishnuj <ervishnuj@gmail.com>
2026-06-26 21:34:24 +0530

    Minimum Time Visiting All Points

---
+++ MinimumTimeVisitingAllPoints.java
@@ -0,0 +1,10 @@
+public class MinimumTimeVisitingAllPoints {
+    public int minTimeToVisitAllPoints(int[][] points) {
+     int res=0;
+     int n=points.length;
+     for(int i=1;i<n;i++){
+        res+=Math.max(Math.abs(points[i][0]-points[i-1][0]),Math.abs(points[i][1]-points[i-1][1]));
+     }   
+     return res;
+    }
+}
