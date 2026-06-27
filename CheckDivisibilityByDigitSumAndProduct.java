aff3a4a36c78333b239b14629408acbc53fd7cfd
ervishnuj <ervishnuj@gmail.com>
2026-06-27 18:19:31 +0530

    CheckDivisibilityByDigitSumAndProduct

---
+++ CheckDivisibilityByDigitSumAndProduct.java
@@ -0,0 +1,14 @@
+public class CheckDivisibilityByDigitSumAndProduct {
+    public boolean checkDivisibility(int num) {    
+        int sum=0;
+        int mul=1;
+        int n=num;
+        while(n>0){
+            int temp=n%10;
+            sum+=temp;
+            mul*=temp;
+            n/=10;
+        }
+        return num%(sum+mul)==0;
+    }
+}
