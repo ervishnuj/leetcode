850770c1675b4574bdd1e5d7a1045771015f8b15
ervishnuj <ervishnuj@gmail.com>
2026-06-27 13:32:40 +0530

    Count The Digits That Divide A Number

---
+++ CountTheDigitsThatDivideANumber.java
@@ -0,0 +1,14 @@
+public class CountTheDigitsThatDivideANumber {
+     public int countDigits(int num) {
+        int count=0;
+        int original=num;
+        while(num>0){
+            int digit=num%10;
+            if(digit!=0&&original%digit==0)cout++;
+            
+            num/=10;
+        }
+        
+        return count;
+    }
+}
