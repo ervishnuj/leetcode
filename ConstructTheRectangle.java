f8024b0768e915007ed3f6053e61f09ab748aa9f
ervishnuj <ervishnuj@gmail.com>
2026-06-28 22:17:05 +0530

    Construct the Rectangle

---
+++ ConstructTheRectangle.java
@@ -0,0 +1,7 @@
+public class ConstructTheRectangle {
+    public int[] constructRectangle(int area) {
+        int width=(int)Math.sqrt(area);
+        while(area%width!=0)width--;
+        return new int[]{area/width,width};
+    }
+}
