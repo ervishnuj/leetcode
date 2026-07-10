a7df12cf53c9da99593ae5546e373714a21a17b6
ervishnuj <110319808+ervishnuj@users.noreply.github.com>
2026-07-10 22:58:29 +0530

    Create RecentCounter.java

---
+++ RecentCounter.java
@@ -0,0 +1,25 @@
+class RecentCounter {
+
+ArrayList<Integer> list;
+    public RecentCounter() {
+        list= new ArrayList<>();
+    }
+    
+    public int ping(int t) {
+        list.add(t);
+
+        while (list.get(0) < t - 3000) {
+
+            list.remove(0);
+
+        }
+
+        return list.size();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
