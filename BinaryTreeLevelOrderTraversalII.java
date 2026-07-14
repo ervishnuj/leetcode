c0fc30dfe2aba7665fd87c3fb241dcbcd0effec8
ervishnuj <ervishnuj@gmail.com>
2026-07-14 18:39:29 +0530

    Binary Tree Level Order Traversal II

--- BinaryTreeLevelOrderTraversalII.java
+++ BinaryTreeLevelOrderTraversalII.java
@@ -4,37 +4,65 @@ import java.util.List;
 import java.util.Queue;
 
 public class BinaryTreeLevelOrderTraversalII {
-    List<List<Integer>> ans;
+    // List<List<Integer>> ans;
 
-    public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        ans = new ArrayList<>();
-        if (root == null)
+    // public List<List<Integer>> levelOrderBottom(TreeNode root) {
+    // ans = new ArrayList<>();
+    // if (root == null)
+    // return ans;
+
+    // TreeNode node = root;
+    // Queue<TreeNode> queue = new LinkedList<>();
+    // queue.add(node);
+
+    // while (!queue.isEmpty()) {
+
+    // Queue<TreeNode> temp = new LinkedList();
+    // List<Integer> list = new ArrayList<>();
+    // while (!queue.isEmpty()) {
+    // TreeNode treeNode = queue.poll();
+    // System.out.println(treeNode.val);
+    // // List<Integer> list=new ArrayList<>();
+    // list.add(treeNode.val);
+    // if (treeNode.left != null)
+    // temp.add(treeNode.left);
+    // if (treeNode.right != null)
+    // temp.add(treeNode.right);
+    // }
+    // queue = temp;
+    // ans.add(0, list);
+    // }
+    // return ans;
+    // }
+    public List<List<Integer>> levelOrderBottom(TreeNode node) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (node == null)
             return ans;
-
-        TreeNode node = root;
+        // TreeNode node=root;
         Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(node);
+        queue.offer(node);
 
         while (!queue.isEmpty()) {
 
-            Queue<TreeNode> temp = new LinkedList();
+            // Queue<TreeNode> temp=new LinkedList();
             List<Integer> list = new ArrayList<>();
-            while (!queue.isEmpty()) {
+            int size = queue.size();
+            // while(!queue.isEmpty()){
+            for (int i = 0; i < size; i++) {
                 TreeNode treeNode = queue.poll();
-                System.out.println(treeNode.val);
+                // System.out.println(treeNode.val);
                 // List<Integer> list=new ArrayList<>();
                 list.add(treeNode.val);
                 if (treeNode.left != null)
-                    temp.add(treeNode.left);
+                    queue.offer(treeNode.left);
                 if (treeNode.right != null)
-                    temp.add(treeNode.right);
+                    queue.offer(treeNode.right);
             }
-            queue = temp;
-            ans.add(0, list);
+            // queue=temp;
+            ans.addFirst(list);
         }
         return ans;
     }
-
 }
 
 // Definition for a binary tree node.
