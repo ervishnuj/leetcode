d68cab2ec8e8f34835384bcabd78b4696c32455d
ervishnuj <ervishnuj@gmail.com>
2026-07-14 21:31:39 +0530

    Populating Next Right Pointers in Each Node II=> level order traversal of binary tree technic used  to solve the problem.

---
+++ PopulatingNextRightPointersInEachNodeII.java
@@ -0,0 +1,51 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class PopulatingNextRightPointersInEachNodeII {
+    public Node connect(Node root) {
+        if (root == null)
+            return root;
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+                if (i + 1 != size) {
+                    node.next = queue.peek();
+                } else {
+                    node.next = null;
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return root;
+    }
+}
+
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {
+    }
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
