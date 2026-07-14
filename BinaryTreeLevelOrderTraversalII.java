684388d29ef2174e62a042f24148c34f9efe13ed
ervishnuj <ervishnuj@gmail.com>
2026-07-14 18:16:07 +0530

    Binary Tree Level Order Traversal II

---
+++ BinaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeLevelOrderTraversalII {
+    List<List<Integer>> ans;
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        ans = new ArrayList<>();
+        if (root == null)
+            return ans;
+
+        TreeNode node = root;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+
+            Queue<TreeNode> temp = new LinkedList();
+            List<Integer> list = new ArrayList<>();
+            while (!queue.isEmpty()) {
+                TreeNode treeNode = queue.poll();
+                System.out.println(treeNode.val);
+                // List<Integer> list=new ArrayList<>();
+                list.add(treeNode.val);
+                if (treeNode.left != null)
+                    temp.add(treeNode.left);
+                if (treeNode.right != null)
+                    temp.add(treeNode.right);
+            }
+            queue = temp;
+            ans.add(0, list);
+        }
+        return ans;
+    }
+
+}
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
