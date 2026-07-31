0211bceebc47dfe7fef70f76050cce5f4582ed27
ervishnuj <110319808+ervishnuj@users.noreply.github.com>
2026-07-31 23:31:54 +0530

    Implement maximumTripletValue method in MaximumValueOfAnOrderedTripletI

---
+++ MaximumValueOfAnOrderedTripletI.java
@@ -0,0 +1,14 @@
+class MaximumValueOfAnOrderedTripletI {
+    public long maximumTripletValue(int[] nums) {
+        int n=nums.length;long max=0;
+        
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+                    max=Math.max((long)(nums[i]-nums[j])*nums[k],max);
+                }
+            }
+        }
+        return max;
+    }
+}
