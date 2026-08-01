7d1fa00ad34072ca2ecd5b30e4664bc78f49b7d1
ervishnuj <110319808+ervishnuj@users.noreply.github.com>
2026-08-01 21:24:21 +0530

    Implement AntOnTheBoundary class with returnToBoundaryCount method

---
+++ AntOnTheBoundary.java
@@ -0,0 +1,11 @@
+class AntOnTheBoundary {
+    public int returnToBoundaryCount(int[] nums) {
+        int sum=nums[0];
+        int count=0;
+        for(int i=1;i<nums.length;i++){
+            sum+=nums[i];
+            if(sum==0)count++;
+        }
+        return count;
+    }
+}
